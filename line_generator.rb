938913ff84df0938d6272a39f0026f7b31824a63
Jeff Gandt <jeff.gandt@gmail.com>
2012-02-24 16:58:00 -0500

    master remove superfluous comments

--- line_generator.rb
+++ line_generator.rb
@@ -17,20 +17,6 @@ module LineGenerator
 
       @finger = YAML::load(File.open( 'finger_key_mapping.yml') )
       
-=begin
-      @finger[0] = ['a', '~', '`', '1', '!']
-      @finger[1] = ['s', '2', '@']
-      @finger[2] = ['d', '3', '#']
-      @finger[3] = ['f', '4', '$', '5', '%']
-
-      @finger[4] = ['j', '6', '^', '7', '&']
-      @finger[5] = ['k', '8', '(', '9', '*', ',', '<']
-      @finger[6] = ['l', '0', ')', '.', '>']
-      @finger[7] = [';', ':', '{', '[', '}', ']', '-', '_' ]
-      @finger[8] = [';', ':', '/', '?', '=', '+', '|', '\\' ]
-      @finger[9] = [';', ':', '"', "'", '/', '?', '-', '_', '{', '[', '}', ']', '=', '+', '|', '\\' ]
-=end
-
       @random_index = Random.rand(@finger.size)
       @keys = @finger[@random_index]
     end
